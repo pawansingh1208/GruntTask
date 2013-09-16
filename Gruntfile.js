12813b41724c424a43fe92d03f62775a48296e48
pawan <pawan.kumar@manusis.com>
2013-09-16 15:11:04 +0530

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+
+        karma: {
+            /*e2e: {
+                configFile: 'karma-e2e.conf.js',
+                singleRun: true
+            },*/
+            unit: {
+                configFile: 'karma.conf.js',
+                singleRun: true
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-karma:e2e');
+
+};
