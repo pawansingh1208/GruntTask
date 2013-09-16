d97b4499482a514ff4ea80c848c9683957ca4c1d
pawan <pawan.kumar@manusis.com>
2013-09-16 15:00:01 +0530

    first commit

---
+++ test/scenario.js
@@ -0,0 +1,11 @@
+describe('addition', function () {
+    'use strict';
+
+    it('should return 4 when adding 2 and 2', function () {
+        expect(2 + 2).toBe(4);
+    });
+
+    it('should return 6 when adding 2 and 4', function () {
+        expect(2 + 4).toBe(6);
+    });
+});
