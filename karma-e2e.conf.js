9aeedcffe4b44209842130de30181577590b29d1
pawan <pawan.kumar@manusis.com>
2013-09-18 17:59:47 +0530

    grunt

--- karma-e2e.conf.js
+++ karma-e2e.conf.js
@@ -1,4 +1,3 @@
-// Karma E2E configuration
 
 // base path, that will be used to resolve files and exclude
 basePath = '';
@@ -31,7 +30,6 @@ reporters = ['junit'];
 preprocessors = {
     '**/*.js': 'coverage'
 };
-
 junitReporter = {
     outputFile : 'results.xml',
     suite : 'e2e'
@@ -49,7 +47,6 @@ logLevel = LOG_INFO;
 
 // enable / disable watching file and executing tests whenever any file changes
 autoWatch = false;
-
 // Start these browsers, currently available:
 // - Chrome
 // - ChromeCanary
@@ -59,7 +56,6 @@ autoWatch = false;
 // - PhantomJS
 // - IE (only Windows)
 browsers = ['Chrome'];
-
 // If browser does not capture in given timeout [ms], kill it
 captureTimeout = 35000;
 
