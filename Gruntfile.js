469a1d51b4f0b6e3b742d2e13b65f28e581a340e
pawan <pawan.kumar@manusis.com>
2013-09-18 17:05:19 +0530

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,10 @@ module.exports = function (grunt) {
     grunt.initConfig({
 
         karma: {
-            /*e2e: {
+            e2e: {
                 configFile: 'karma-e2e.conf.js',
                 singleRun: true
-            },*/
+            },
             unit: {
                 configFile: 'karma-e2e.conf.js',
                 singleRun: true
